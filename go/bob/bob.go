c27c5a16e7efd9e8c8791bf797d47dc49db55594
Patrick Ringl <patrick_@freenet.de>
2017-08-31 21:24:44 +0200

    exercise: 'bob'

---
+++ go/bob/bob.go
@@ -0,0 +1,27 @@
+// Package bob provides the implementation of exercism's "bob" exercise.
+package bob
+
+// import path
+import (
+	"strings"
+)
+
+// consts
+const testVersion = 3
+
+// Hey implements Bob's lackadaisical teenager responses depending on input
+func Hey(input string) string {
+	input = strings.TrimSpace(input)
+	if len(input) == 0 {
+		return "Fine. Be that way!"
+	} else {
+		endsWith := input[len(input)-1]
+		if strings.ToUpper(input) == input && strings.ContainsAny(input, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+			return "Whoa, chill out!"
+		} else if endsWith == '?' {
+			return "Sure."
+		} else {
+			return "Whatever."
+		}
+	}
+}
